90bcfa3f4c083cebcf30f1d0e74d140027ec4273
Stone Man <ruipedronetoreis12@gmail.com>
2019-02-23 10:18:01 +0000

    More structural changes

--- validation.h
+++
@@ -1,8 +0,0 @@
-#ifndef VALIDATION_H
-#define VALIDATION_H
-
-int         is_valid_client         (char*);
-int         is_valid_product        (char*);
-int         is_valid_sale           (char**,int);   //Recieves a tokenized string.
-
-#endif
