5a42bff1eb36c1058a38c3ad1ab56d7a986a2009
Stone Man <ruipedronetoreis12@gmail.com>
2019-02-24 00:18:23 +0000

    New semi-finished parser aprox. 35% more efficient.

--- c-ref/include/validation.h
+++ c-ref/include/validation.h
@@ -1,8 +1,14 @@
 #ifndef VALIDATION_H
 #define VALIDATION_H
 
-int         is_valid_client         (char*);
-int         is_valid_product        (char*);
-int         is_valid_sale           (char**,int);
+#define VRF_CLIENT 0
+#define VRF_PRODUCT 1
+#define VRF_SALE 2
+
+typedef struct verifier *VRF_OBJ;
+
+VRF_OBJ make_vrf (void);
+int vrf_obj_str (VRF_OBJ,char*,int);
+void destroy_vrf (VRF_OBJ);
 
 #endif
--- c-ref/src/validation.c
+++ c-ref/src/validation.c
@@ -1,47 +1,119 @@
 #include "validation.h"
 #include <glib.h>
+#include <stdio.h>
+
+typedef int (*scompare) (char*);
+
+typedef struct verifier {
+    scompare fa[7];
+} *VRF_OBJ;
 
 // USEFUL MACROS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#define     is_between(x,min,max)   ((x>=min) && (x<=max))
 
-// PUBLIC METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-int         is_valid_client         (char*);
-int         is_valid_product        (char*);
-int         is_valid_sale           (char**,int);
+#define     is_between(x,min,max)   ((x>=min) && (x<=max))
+#define     min(a,b)    ((a)<(b)?(a):(b))
+
+// Public methods
+VRF_OBJ make_vrf (void);
+int vrf_obj_str (VRF_OBJ,char*,int);
+void destroy_vrf (VRF_OBJ);
+
+// Private METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+int is_valid_client (char*);
+int is_valid_product (char*);
+int is_valid_sale (VRF_OBJ,char*);
+int is_valid_price (char*);
+int is_valid_units (char*);
+int is_valid_promo (char*);
+int is_valid_month (char*);
+int is_valid_filial (char*);
 
 // CODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-int         is_valid_client         (char* s)
-    {
-        return (
-            g_ascii_isupper(s[0])                                           //Vejo se a primeira letra e maiuscula
-            && is_between(atoi(s+1),1000,5000)                      //Vejo se esse numero esta entre 1000 e 5000
-            && (s[5] == '\0')                                       //Vejo se a frase de facto termina quando eu acho que ela deve terminar
-        );
-    }
+VRF_OBJ make_vrf (void)
+{
+    VRF_OBJ v = g_malloc(sizeof(struct verifier));
+    v->fa[0] = is_valid_product;
+    v->fa[1] = is_valid_price;
+    v->fa[2] = is_valid_units;
+    v->fa[3] = is_valid_promo;
+    v->fa[4] = is_valid_client;
+    v->fa[5] = is_valid_month;
+    v->fa[6] = is_valid_filial;
+    return v;
+}
 
-int         is_valid_product        (char* s)
-    {
-        return (
-            g_ascii_isupper(s[0])
-            && g_ascii_isupper(s[1])
-            && is_between(atoi(s+2),1000,9999)
-            && (s[6] == '\0')
-        );
-    }
+int vrf_obj_str (VRF_OBJ v,char* str, int c)
+{
 
-int         is_valid_sale           (char** placer, int N)
-    {
-        int r = 1;
-        if (N == 7) {
-            r = is_valid_product(placer[0])
-                && is_between(atof(placer[1]),0.0,999.99)
-                && is_between(atoi(placer[2]),1,200)
-                && (placer[3][0] == 'N' || placer[3][0] == 'P')
-                && is_valid_client(placer[4])
-                && is_between(atoi(placer[5]),1,12)
-                && is_between(atoi(placer[6]),1,3);
+    int r = 0;
+    if(v) {
+        switch(c) {
+            case 0: r = (*v->fa[4])(str); break;
+            case 1: r = (*v->fa[0])(str); break;
+            case 2: r = is_valid_sale(v,str); break;
         }
-        else
-            r = 0;
-        return r;
     }
+    return r;
+}
+
+void destroy_vrf (VRF_OBJ v)
+{
+    g_free(v);
+}
+
+int is_valid_client (char *s)
+{
+    return (
+        g_ascii_isupper(s[0])          
+        && is_between(atoi(s+1),1000,5000)  
+        && (s[5] == '\0')              
+    );
+}
+
+int is_valid_product (char *s)
+{
+    return (
+        g_ascii_isupper(s[0])
+        && g_ascii_isupper(s[1])
+        && is_between(atoi(s+2),1000,9999)
+        && (s[6] == '\0')
+    );
+}
+int is_valid_sale (VRF_OBJ v, char* s)
+    {
+        int i,tmp,r = 1;
+        char *token = strtok(s," \n");
+        
+        for (i = 0; r && token && i < 7; i++, token = strtok(NULL," \n")){
+            tmp = (*v->fa[i])(token);
+            r = min(r,tmp);
+        }
+
+        return ( ( token && i >= 7 ) ? 0 : r  );
+    }
+
+// Private
+int is_valid_price  (char* s)
+{
+    return is_between(atof(s),0.0,999.99);
+}
+
+int is_valid_units  (char* s)
+{
+    return is_between(atoi(s),1,200);
+}
+
+int is_valid_promo  (char* s)
+{
+    return (*s == 'N' || *s == 'P');
+}
+
+int is_valid_month  (char* s)
+{
+    return is_between(atoi(s),1,12);
+}
+
+int is_valid_filial (char* s)
+{
+    return is_between(atoi(s),1,3);
+}
