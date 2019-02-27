4fa6349c0c3ef4d48c58a5f96f99487d770a6332
André Figueiredo <haandree733@hotmail.com>
2019-02-27 23:25:00 +0000

    Agora, já compara também no fim do ficheiro

--- c-ref/src/max_length.c
+++ c-ref/src/max_length.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h> // Podes usar esta biblioteca? Só está aqui para não dar warning no exit
-#define MAX_BUFFER 256 // Quantidade de bytes que vão ser lidos de uma só vez
+#define MAX_BUFFER 512 // Quantidade de bytes que vão ser lidos de uma só vez
 
 int max_length() {
     int at, i, max;
 
     char buff [MAX_BUFFER];
-    FILE * fp = fopen("tests/Vendas_1M.txt","r"); // Ficheiro a ler
+    FILE * fp = fopen("linhas.txt","r"); // Ficheiro a ler
 
     at=max=0;
     if (fp){
     	while (fread(buff, sizeof(char), MAX_BUFFER, fp)) { // Usei o fread, porque podemos ler a quantidade de bits que quisermos..
 					for (i = 0; i < MAX_BUFFER ; ++i) {
-	    	           	if (buff[i]=='\n') {
+	    	           	if (buff[i]=='\n' || buff[i]=='\0') {
 	    	           		max=(max<at)? at : max;
 							at=-1;
 	    	           	}
@@ -22,6 +22,7 @@ int max_length() {
     }
     else exit(1); // Caso o ficheiro não exista	
     
+    printf("%d\n", max-1);
     fclose(fp);
     
     return max-1;
