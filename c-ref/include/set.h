77e5df3f909c01cab9c090cdf597cf217c5895e6
Rui Reis <ruipedronetoreis12@gmail.com>
2019-03-15 09:54:19 +0000

    Rr/tarefa91012 (#25)
    * added a set
    
    * update set module file
    
    * structural changes, Makefile updated
    
    * small

--- c-ref/include/set.h
+++ c-ref/include/set.h
@@ -6,13 +6,21 @@
 
 typedef struct set *StrSet;
 
+typedef void (*f_foreach)(void *, void *, void *);
+
+typedef void (*f_update)(void *, void *);
+
+typedef void (*f_maker)(void);
+
 StrSet strset_make(freefunc ff);
 void strset_destroy(StrSet set);
-int strset_add(StrSet set, char *elem);
+int strset_add(StrSet set, char *elem, void *value);
 int strset_remove(StrSet set, char *elem);
-void strset_foreach(StrSet set, void (*fe)(void *, void *, void *), void *user_data);
+void strset_foreach(StrSet set, f_foreach fer, void *user_data);
 int strset_contains(StrSet set, char *elem);
 int strset_size(StrSet set);
+int strset_update_elem(StrSet set, char *elem, void (*f_up)(void *, void *), void *user_data);
+void *strset_value_of(StrSet set, char *elem);
 char **strset_dump(StrSet set, size_t *n);
 char **strset_dump_ordered(StrSet set, fcompar fc, size_t *n);
 
