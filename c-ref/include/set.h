0c9759075c65e36ff40e825c10cd4ea8a091c90c
Syrayse <ruipedronetoreis12@gmail.com>
2019-04-04 20:03:27 +0100

    new set

--- c-ref/include/set.h
+++ c-ref/include/set.h
@@ -6,44 +6,27 @@
 #ifndef SET_H
 #define SET_H
 
-#include "util.h"
 #include <glib.h>
 
-typedef struct set *StrSet;
-
-/**
- * \brief Redefine uma função que deve ser usada num metódo foreach.
- */
-typedef void (*f_foreach)(void *, void *, void *);
+typedef struct set *Set;
 
 /**
  * \brief Redefine uma função que deve ser usada como atualizador de um elemento.
  */
-typedef void (*f_update)(void *, void *);
+typedef void (*f_update)(gpointer, gpointer);
 
 /**
  * \brief Redefine uma função que deve ser usada como criadora da instância pretendida.
  */
-typedef void *(*f_maker)();
+typedef gpointer (*f_maker)();
 
-/**
- * \brief Redefine uma função que deve ser usada de modo a verificar se um elemento é vazio.
- */
-typedef int (*f_empty)(void *);
-
-StrSet strset_make(freefunc ffkey, freefunc ffvalue, f_maker fm, f_update fu, f_empty fe);
-void strset_destroy(StrSet set);
-int strset_add(StrSet set, void *elem, void *user_data);
-int strset_remove(StrSet set, void *elem);
-void strset_foreach(StrSet set, f_foreach fer, void *user_data);
-int strset_contains(StrSet set, void *elem);
-int strset_size(StrSet set);
-void *strset_lookup(StrSet set, void *key);
-void *strset_value_of(StrSet set, void *elem);
-char **strset_dump_ordered(StrSet set, int *n);
-int strset_get_not_init_n(StrSet set);
-char **strset_generic_dump(StrSet set, f_foreach ffor, int *n, int flag);
-char **strset_dump_if(StrSet set, fcompare fc, int *n);
-char **strset_dump_n_ordered(StrSet set, int n);
+Set set_make(GHashFunc hash_f, GEqualFunc key_equal_f, freefunc ffkey, freefunc ffvalue, f_maker fm, f_update fu);
+void set_destroy(Set set);
+int set_add(Set set, gpointer key, gpointer user_data);
+int set_remove(Set set, gpointer key);
+void set_foreach(Set set, GHFunc fe, gpointer user_data);
+int set_contains(Set set, gpointer key);
+int set_size(Set set);
+gpointer set_lookup(Set set, gpointer key);
 
 #endif
--- c-ref/src/lib/set.c
+++ c-ref/src/lib/set.c
@@ -1,177 +1,102 @@
 #include "set.h"
-#include "util.h"
-#include "gArray.h"
-
-/* ------------------------------------------------------------------------------ */
-
-/* Metodos publicos */
-StrSet strset_make(freefunc ffkey, freefunc ffvalue, f_maker fm, f_update fu, f_empty fe);
-void strset_destroy(StrSet set);
-int strset_add(StrSet set, void *elem, void *user_data);
-int strset_remove(StrSet set, void *elem);
-void strset_foreach(StrSet set, f_foreach fer, void *user_data);
-int strset_contains(StrSet set, void *elem);
-int strset_size(StrSet set);
-void *strset_lookup(StrSet set, void *key);
-void *strset_value_of(StrSet set, void *elem);
-char **strset_dump_ordered(StrSet set, int *n);
-int strset_get_not_init_n(StrSet set);
-char **strset_generic_dump(StrSet set, f_foreach ffor, int *n, int flag);
-char **strset_dump_if(StrSet set, fcompare fc, int *n);
-char **strset_dump_n_ordered(StrSet set, int n);
-
-/* Metodos privados */
-static void foreach_add(void *key, void *value, void *user_data);
+#include <glib.h>
 
 /* ------------------------------------------------------------------------------ */
 
 typedef struct set
 {
-    int not_init;
     GHashTable *table;
     f_maker fm;
     f_update fu;
-    f_empty fe;
-} * StrSet;
+} * Set;
 
 /* ------------------------------------------------------------------------------ */
 
-StrSet strset_make(freefunc ffkey, freefunc ffvalue, f_maker fm, f_update fu, f_empty fe)
+/* Metodos publicos */
+Set set_make(GHashFunc hash_f, GEqualFunc key_equal_f, freefunc ffkey, freefunc ffvalue, f_maker fm, f_update fu);
+void set_destroy(Set set);
+int set_add(Set set, gpointer key, gpointer user_data);
+int set_remove(Set set, gpointer key);
+void set_foreach(Set set, f_foreach fer, gpointer user_data);
+int set_contains(Set set, gpointer key);
+int set_size(Set set);
+gpointer set_lookup(Set set, gpointer key);
+
+/* Metodos privados */
+
+/* ------------------------------------------------------------------------------ */
+
+Set set_make(GHashFunc hash_f, GEqualFunc key_equal_f, freefunc ffkey, freefunc ffvalue, f_maker fm, f_update fu)
 {
-    StrSet set = g_malloc(sizeof(struct set));
-    set->not_init = 0;
-    set->table = g_hash_table_new_full(g_str_hash, g_str_equal, ffkey, ffvalue);
+    set set = g_malloc(sizeof(struct set));
+
+    set->table = g_hash_table_new_full(hash_f, key_equal_f, ffkey, ffvalue);
+
     set->fm = fm;
+
     set->fu = fu;
-    set->fe = fe;
+
     return set;
 }
 
-void strset_destroy(StrSet set)
+void set_destroy(Set set)
 {
     if (set)
     {
         g_hash_table_destroy(set->table);
+
         g_free(set);
     }
 }
 
-int strset_get_not_init_n(StrSet set)
+int set_add(Set set, gpointer key, gpointer user_data)
 {
-    return set->not_init;
-}
-
-int strset_add(StrSet set, void *elem, void *user_data)
-{
-    if (!elem)
-        return -1;
-
     int r = 1;
-    void *val = NULL;
+    gpointer dumkey, dumvalue;
+    dumkey = dumvalue = NULL;
 
-    if (!(val = g_hash_table_lookup(set->table, elem)))
+    if (!g_hash_table_lookup_extended(set->table, key, &dumkey, &dumvalue))
     {
+        r = 0;
+
         if (set->fm)
         {
-            val = (*set->fm)();
-            set->not_init++;
-            g_hash_table_insert(set->table, elem, val);
+            dumvalue = (*set->fm)();
         }
+
+        g_hash_table_insert(set->table, key, dumvalue);
     }
 
-    if (user_data && set->fu)
+    if (dumvalue && user_data && set->fu)
     {
-        if (set->fe && (*set->fe)(val))
-        {
-            set->not_init--;
-        }
 
-        (*set->fu)(val, user_data);
+        (*set->fu)(dumvalue, user_data);
     }
 
     return r;
 }
 
-int strset_remove(StrSet set, void *elem)
+int set_contains(Set set, gpointer key)
 {
-    return g_hash_table_remove(set->table, elem);
+    return g_hash_table_contains(set->table, key);
 }
 
-void strset_foreach(StrSet set, void (*fe)(void *, void *, void *), void *user_data)
+int set_remove(Set set, gpointer key)
+{
+    return g_hash_table_remove(set->table, key);
+}
+
+void set_foreach(Set set, GHFunc fe, gpointer user_data)
 {
     g_hash_table_foreach(set->table, fe, user_data);
 }
 
-int strset_contains(StrSet set, void *elem)
-{
-    return g_hash_table_contains(set->table, elem);
-}
-
-int strset_size(StrSet set)
+int set_size(Set set)
 {
     return g_hash_table_size(set->table);
 }
 
-char **strset_dump_ordered(StrSet set, int *n)
+gpointer set_lookup(set set, void *key)
 {
-    return strset_generic_dump(set, foreach_add, n, 0);
-}
-
-int strset_update_elem(StrSet set, char *elem, void (*f_up)(void *, void *), void *user_data)
-{
-    void *tmp;
-
-    if ((tmp = g_hash_table_lookup(set->table, elem)))
-    {
-        (*f_up)(tmp, user_data);
-    }
-
-    return tmp ? 1 : 0;
-}
-
-void *strset_lookup(StrSet set, void *key)
-{
-    return key ? g_hash_table_lookup(set->table, key) : NULL;
-}
-
-char **strset_generic_dump(StrSet set, f_foreach ffor, int *n, int flag)
-{
-    char **r;
-    GrowingArray ga = garray_make(sizeof(char *), NULL);
-
-    strset_foreach(set, ffor, ga);
-
-    if (!flag)
-        garray_sort(ga, mystrcmp);
-
-    r = (char **)garray_dump_elems(ga, NULL, n);
-
-    garray_destroy(ga);
-
-    return r;
-}
-
-char **strset_dump_n_ordered(StrSet set, int n)
-{
-    if (n <= 0)
-        return NULL;
-
-    int trash;
-    char **r;
-    GrowingArray ga = garray_make_sized(sizeof(char *), n, NULL);
-
-    strset_foreach(set, foreach_add, ga);
-
-    garray_sort(ga, mystrcmp);
-
-    r = (char **)garray_dump_elems(ga, NULL, &trash);
-
-    garray_destroy(ga);
-
-    return r;
-}
-
-static void foreach_add(void *key, void *value, void *user_data)
-{
-    garray_add((GrowingArray)user_data, g_strdup((char *)key));
+    return g_hash_table_lookup(set->table, key);
 }
